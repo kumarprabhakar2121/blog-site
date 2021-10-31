cd32db73c83d307dfcc6e9dffb3c3d7feb41bdc9
kumarprabhakar2121 <kumarprabhakar2121@gmail.com>
2021-10-31 21:57:58 +0530

    ch 11 done

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const contactContent =
   "CONTACT CONTACT Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = [];
 
 app.set("view engine", "ejs");
 
@@ -28,17 +29,19 @@ app.get("/about", function (req, res) {
   res.render("about", { aboutPageContent: aboutContent });
 });
 
-app.get("/compose",function(req,res){
+app.get("/compose", function (req, res) {
   res.render("compose");
-})
+});
 
-app.post("/compose",function(req,res){
-  const post ={
+app.post("/compose", function (req, res) {
+  const post = {
     title: req.body.postTitle,
-    body: req.body.postBody
-  }
-  console.log(post);
-})
+    body: req.body.postBody,
+  };
+  posts.push(post);
+  console.log(posts);
+  res.redirect("/");
+});
 
 app.listen(3000, function () {
   console.log("Server started on localhost port 3000");
